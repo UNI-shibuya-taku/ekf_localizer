2d5a528441503b3025bfd59b6f061e9dbc7837d8
UNI-shibuya-taku <s.takuuuuu@gmail.com>
2023-10-21 14:40:56 +0900

    add tf

---
+++ include/tf/dynamic_tf_cub.h
@@ -0,0 +1,31 @@
+#ifndef __DYNAMIC_TF_CUB_H
+#define __DYNAMIC_TF_CUB_H
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+
+class DynamicTfCub
+{
+public:
+    DynamicTfCub();
+    void process();
+
+private:
+    //method
+    void pub_dynamic_tf();
+    //parameter
+
+    //member
+    ros::NodeHandle nh;
+    ros::NodeHandle private_nh;
+    ros::Publisher pub_next_waypoint;
+    ros::Subscriber sub_current_pose;
+
+    geometry_msgs::PoseStamped current_pose;
+    tf2_ros::TransformBroadcaster dynamic_br_;
+};
+
+#endif
---
+++ src/dynamic_tf_cub.cpp
@@ -0,0 +1,43 @@
+#include "ekf_localizer/dynamic_tf_cub.h"
+DynamicTfCub::DynamicTfCub():private_nh("~")
+{
+    /*subscriber*/
+    sub_current_pose = nh.subscribe("/ekf_pose",10,&NextWaypointCreator::current_pose_callback,this);
+}
+void DynamicTfCub::current_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg)
+{
+    current_pose = *msg;
+    pub_dynamic_tf();
+}
+
+void DynamicTfCub::pub_dynamic_tf()
+{
+    geometry_msgs::TransformStamped transformStamped;
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "map";
+    transformStamped.child_frame_id = "base_link";
+    transformStamped.transform.translation.x = current_pose.pose.position.x;
+    transformStamped.transform.translation.y = current_pose.pose.position.y;
+    transformStamped.transform.translation.z = 0.0;
+
+    // tf2::Quaternion q;
+    // q.setRPY(0, 0, 0.0); // YAW
+    transformStamped.transform.rotation.x = current_pose.pose.orientation.x;
+    transformStamped.transform.rotation.y = current_pose.pose.orientation.y;
+    transformStamped.transform.rotation.z = current_pose.pose.orientation.z;
+    transformStamped.transform.rotation.w = current_pose.pose.orientation.w;
+    dynamic_br_.sendTransform(transformStamped);
+}
+
+void DynamicTfCub::process()
+{
+    ros::Spin();
+}
+
+int main (int argc,char **argv)
+{
+    ros::init(argc, argv, "dynamic_tf_cub");
+    DynamicTfCub dynamic_tf_cub;
+    dynamic_tf_cub.process();
+    return 0;
+}
